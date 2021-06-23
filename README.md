392c9b8adab762e50ce237827595158aa9535379
S_ghanbary98 <67980207+Sherv1360@users.noreply.github.com>
2021-06-23 12:12:53 +0100

    first commit of task

---
+++ README.md
@@ -0,0 +1,3 @@
+# String Interpolation and Concatenation
+
+#In this task the user inputs a name. The program then inputs that name via both interpolation and concatenation
---
+++ string_concantenation_interpolation.py
@@ -0,0 +1,15 @@
+
+
+## Task 1
+
+name = "random"
+name = input("What is your first name?")
+
+print("Hi" + " " + name.capitalize() + ", " + "welcome to python!!")
+
+
+
+## Task 2
+full_name = 'random person'
+full_name = input("What is your full name?")
+print("Hi {} {}, welcome to python!!".format(full_name.split()[0].capitalize(), full_name.split()[1].capitalize()))
